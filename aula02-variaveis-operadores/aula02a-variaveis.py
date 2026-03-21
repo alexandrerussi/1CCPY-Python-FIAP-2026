058811c18912cb956a7ae5febc1f310c7f50b1fd
alexandrerussi <alexandrerussijunior@gmail.com>
2026-03-20 22:32:00 -0300

    aula 2 variaveis e operadores

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,34 @@
+print("ola mundo")
+
+print(7 + 4)
+print("7 + 4")
+print("7" + "4") # CONCATENAÇÃO DE STRINGS
+
+# Comentários de 1 linha em python
+'''
+Comentários de
+múltiplas 
+linhas
+'''
+
+# VARIÁVEIS
+nome = "Alexandre" # string - texto
+idade = 26 # int - numero
+peso = 70.2 # float - num. decimal
+
+print(nome, idade, peso)
+print(f"Oiiii, {nome}!!!!")
+
+# INPUTS - SIMULAÇÃO DE FORMS NO CMD
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+print("Oiii", nome, "! Vc tem", idade, "anos")
+print(f"Oiii {nome}! Vc tem {idade} anos")
+
+nova_idade = idade + 1
+print(nova_idade)
+
+
+
+
